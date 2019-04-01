39e48d12f25344fb25aadb5a1ef67d7c9d2bc200
rafatalosta <32619847+rafatalosta@users.noreply.github.com>
2019-04-01 08:49:37 +0300

    Update Rents.js

--- app/screens/Rents.js
+++ app/screens/Rents.js
@@ -1,27 +1,255 @@
 import React, {Component} from 'react';
 import {
-    View,
+    StyleSheet,
     Text,
-    StyleSheet
+    View, 
+    ScrollView, 
+    Image, 
+    TouchableWithoutFeedback, 
+    Animated, 
+    Dimensions, 
+    SafeAreaView,
+    FlatList
 } from 'react-native';
 
-class Rents extends Component{
+import flatList from '../../data/flatList';
+let SCREEN_WIDTH = Dimensions.get('window').width
+let SCREEN_HEIGHT = Dimensions.get('window').height
+
+var images = [
+    { id: 1, src: require('../images/img3.png') },
+    { id: 2, src: require('../images/img2.jpg') },
+    { id: 3, src: require('../images/img4.jpg') },
+    { id: 4, src: require('../images/imga.jpg') },
+  
+  ]
+
+  class FlatListItem extends Component{
     render(){
-        return(
-            <View style={styles.container}>
-                <Text>Rents</Text>
-            </View>
-        )
+      return(
+        <View style={{flex:1, flexDirection: 'column'}}>
+          <View style={{flex: 1, backgroundColor: '#fff', flexDirection: 'row'}}>
+          <Image
+            source={{uri: this.props.item.imageUrl}}
+            style={{width: 100, height: 100, margin:5}}>
+
+          </Image>
+          <View style={{flex: 1, flexDirection: 'column'}}>
+          <Text style={styles.flatListItem}>{this.props.item.name}</Text>
+          <Text style={styles.flatListItem}>{this.props.item.machineDescription}</Text>
+          <Text style={styles.flatListItem}>{this.props.item.price}</Text>
+
+          </View>
+        </View>
+        <View style={{height: 1, backgroundColor: '#b3ffff'}}>
+
+        </View>
+      </View>
+        
+      )
     }
-}
+  }
+
+class Rents extends Component{
+    constructor() {
+        super()
+        this.state = {
+          activeImage: null
+        }
+      }
+      componentWillMount() {
+        this.allImages = {}
+        this.oldPosition = {}
+        this.position = new Animated.ValueXY()
+        this.dimensions = new Animated.ValueXY()
+        this.animation = new Animated.Value(0)
+        this.activeImageStyle = null
+      }
+    
+      openImage = (index) => {
+    
+        this.allImages[index].measure((x, y, width, height, pageX, pageY) => {
+          this.oldPosition.x = pageX
+          this.oldPosition.y = pageY
+          this.oldPosition.width = width
+          this.oldPosition.height = height
+    
+          this.position.setValue({
+            x: pageX,
+            y: pageY
+          })
+    
+          this.dimensions.setValue({
+            x: width,
+            y: height
+          })
+    
+          this.setState({
+            activeImage: images[index]
+          }, () => {
+            this.viewImage.measure((dx, dy, dWidth, dHeight, dPageX, dPageY) => {
+    
+              Animated.parallel([
+                Animated.timing(this.position.x, {
+                  toValue: dPageX,
+                  duration: 300
+                }),
+                Animated.timing(this.position.y, {
+                  toValue: dPageY,
+                  duration: 300
+                }),
+                Animated.timing(this.dimensions.x, {
+                  toValue: dWidth,
+                  duration: 300
+                }),
+                Animated.timing(this.dimensions.y, {
+                  toValue: dHeight,
+                  duration: 300
+                }),
+                Animated.timing(this.animation, {
+                  toValue: 1,
+                  duration: 300
+                })
+              ]).start()
+            })
+          })
+        })
+      }
+      closeImage = () => {
+        Animated.parallel([
+          Animated.timing(this.position.x, {
+            toValue: this.oldPosition.x,
+            duration: 300
+          }),
+          Animated.timing(this.position.y, {
+            toValue: this.oldPosition.y,
+            duration: 250
+          }),
+          Animated.timing(this.dimensions.x, {
+            toValue: this.oldPosition.width,
+            duration: 250
+          }),
+          Animated.timing(this.dimensions.y, {
+            toValue: this.oldPosition.height,
+            duration: 250
+          }),
+          Animated.timing(this.animation, {
+            toValue: 0,
+            duration: 250
+          })
+        ]).start(() => {
+          this.setState({
+            activeImage: null
+          })
+        })
+      }
+      render() {
+    
+        const activeImageStyle = {
+          width: this.dimensions.x,
+          height: this.dimensions.y,
+          left: this.position.x,
+          top: this.position.y
+        }
+    
+        const animatedContentY = this.animation.interpolate({
+          inputRange: [0, 1],
+          outputRange: [-150, 0]
+        })
+    
+        const animatedContentOpacity = this.animation.interpolate({
+          inputRange: [0, 0.5, 1],
+          outputRange: [0, 1, 1]
+        })
+    
+        const animatedContentStyle = {
+          opacity: animatedContentOpacity,
+          transform: [{
+            translateY: animatedContentY
+          }]
+        }
+    
+        const animatedCrossOpacity = {
+          opacity: this.animation
+        }
+    
+        return (
+          <SafeAreaView style={{ flex: 1 }}>
+            <ScrollView style={{ flex: 1 }} horizontal={true}>
+              {images.map((image, index) => {
+                return (
+                  <TouchableWithoutFeedback
+                    onPress={() => this.openImage(index)}
+                    key={image.id}>
+                    <Animated.View
+                      style={{ height: SCREEN_HEIGHT - 550, width: SCREEN_WIDTH - 250, padding: 10,
+                         paddingTop:20, borderWidth: 0.3, borderColor: '#cc0000' }}
+                    >
+                      <Image
+                        ref={(image) => (this.allImages[index] = image)}
+                        source={image.src}
+                        style={{ flex:1, height: null, width: null, resizeMode: 'cover', borderRadius: 10 }}
+                      />
+                     
+                    </Animated.View>
+                  </TouchableWithoutFeedback>
+                )
+              })}
+            
+            </ScrollView>
+            <View style={{flex: 2, marginTop: 22}}>
+                        <FlatList 
+                          data={flatList}
+                          renderItem={({item, index})=>{
+                            return(
+                            <FlatListItem item={item} index = {index}>
+                            
+                            </FlatListItem>);
+                          }}
+                          >
+
+                        </FlatList>
+
+                      </View>
+            <View style={StyleSheet.absoluteFill}
+              pointerEvents={this.state.activeImage ? "auto" : "none"}
+            >
+              <View style={{ flex: 2, zIndex: 1001 }} ref={(view) => (this.viewImage = view)}>
+                <Animated.Image
+                  source={this.state.activeImage ? this.state.activeImage.src : null}
+                  style={[{ resizeMode: 'cover', top: 0, left: 0, height: null, width: null }, activeImageStyle]}
+                >
+                </Animated.Image>
+                <TouchableWithoutFeedback onPress={() => this.closeImage()}>
+                  <Animated.View style={[{ position: 'absolute', top: 30, right: 30 }, animatedCrossOpacity]}>
+                    <Text style={{ fontSize: 24, fontWeight: 'bold', color: 'black' }}>X</Text>
+                  </Animated.View>
+                </TouchableWithoutFeedback>
+              </View>
+              <Animated.View style={[{ flex: 1, zIndex: 1000, backgroundColor: 'white', padding: 20, paddingTop: 50 }, animatedContentStyle]}>
+                <Text style={{ fontSize: 24, paddingBottom: 10 }}>Unsure Programmer</Text>
+                <Text>Eiusmod consectetur cupidatat dolor Lorem excepteur excepteur. Nostrud sint officia consectetur eu pariatur laboris est velit. Laborum non cupidatat qui ut sit dolore proident.</Text>
+              </Animated.View>
+            </View>
+          </SafeAreaView>
+        );
+      }
+    }
+    
 
 export default Rents;
 
 
 const styles = StyleSheet.create({
-    container:{
+    container: {
       flex: 1,
-      backgroundColor: '#fff'
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    flatListItem:{
+      color: 'black',
+      padding: 10,
+      fontSize: 16,
     }
-    
   });
